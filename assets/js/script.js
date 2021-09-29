bf2ff7c85f2c0643db27e0588f5aacc1d281475f
JacobRyanHanson <jacobryanhanson@gmail.com>
2021-09-28 21:19:18 -0500

    Color code descriptions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,47 @@
+//TODO: Make descriptions editable
+//TODO: Save descriptions to local storage
+
 var date = moment().format('dddd, MMMM Do YYYY');
 document.querySelector("#date").textContent = date;
 
 var container = document.querySelector(".container");
 container.addEventListener("click", updateHandler);
 
+loadPage();
+
 function updateHandler(event) {
     if (event.target.matches(".description")) {
-        auditDescription();
+        auditDescription(event.target);
     } else if (event.target.matches(".save-btn")) {
         saveDescription();
     }
 }
 
-function auditDescription() {
-    
+function auditDescription(descriptionEL) {
+    var timeText = descriptionEL.previousElementSibling.textContent;
+    var time = moment(timeText, "h a");
+
+    descriptionEL.parentElement.classList.remove("background-light");
+    descriptionEL.parentElement.classList.remove("bg-success");
+    descriptionEL.parentElement.classList.remove("bg-danger");
+
+    if (time.format("h a").toUpperCase() === moment().format("h a").toUpperCase()) {
+        descriptionEL.parentElement.classList.add("bg-success");
+    } else if (moment().isAfter(time)) {
+        descriptionEL.parentElement.classList.add("bg-danger");
+    } else {
+        descriptionEL.parentElement.classList.add("background-light");
+    }
 }
 
 function saveDescription() {
 
 }
 
-function loadDescription() {
-
+function loadPage() {
+    var descriptions = document.querySelectorAll(".description");
+    
+    for (var i = 0; i < descriptions.length; i++) {
+        auditDescription(descriptions[i]);
+    }  
 }
