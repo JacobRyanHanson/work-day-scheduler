609d788c279a0093ecf7028c1e4347c4befd3377
JacobRyanHanson <jacobryanhanson@gmail.com>
2021-09-29 21:12:41 -0500

    Fix text input updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-//TODO: Make descriptions editable
 //TODO: Save descriptions to local storage
 
 var date = moment().format('dddd, MMMM Do YYYY');
@@ -6,15 +5,37 @@ document.querySelector("#date").textContent = date;
 
 var container = document.querySelector(".container");
 container.addEventListener("click", updateHandler);
+container.addEventListener("change", updateDescription);
 
 loadPage();
 
 function updateHandler(event) {
     if (event.target.matches(".description")) {
-        auditDescription(event.target);
-    } else if (event.target.matches(".save-btn")) {
+        editDescription(event.target);
+    } else if (event.target.matches(".save-btn") || event.target.matches(".fa-save")) {
         saveDescription();
     }
+}
+
+function editDescription(descriptionEL) {
+    var descriptionText = descriptionEL.textContent;
+    var descriptionInput = document.createElement("input");
+    descriptionInput.value = descriptionText;
+    descriptionEL.textContent = "";
+    descriptionEL.appendChild(descriptionInput, descriptionEL);
+
+    auditDescription(descriptionEL);
+
+    descriptionInput.focus();
+}
+
+function updateDescription(event) {
+    var descriptionInput = event.target;
+    var descriptionEl = descriptionInput.parentElement;
+    var descriptionText = descriptionInput.value;
+
+    descriptionEl.removeChild(descriptionInput);
+    descriptionEl.textContent = descriptionText;
 }
 
 function auditDescription(descriptionEL) {
@@ -27,15 +48,24 @@ function auditDescription(descriptionEL) {
 
     if (time.format("h a").toUpperCase() === moment().format("h a").toUpperCase()) {
         descriptionEL.parentElement.classList.add("bg-success");
+        if (descriptionEL.children) {
+            descriptionEL.firstChild.className = "bg-success";
+        }
     } else if (moment().isAfter(time)) {
         descriptionEL.parentElement.classList.add("bg-danger");
+        if (descriptionEL.children) {
+            descriptionEL.firstChild.className = "bg-danger";
+        }
     } else {
         descriptionEL.parentElement.classList.add("background-light");
+        if (descriptionEL.children) {
+            descriptionEL.firstChild.className = "background-light";
+        }
     }
 }
 
 function saveDescription() {
-
+    console.log("saved")
 }
 
 function loadPage() {
